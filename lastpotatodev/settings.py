4061d8a5709450cde1698a939d87922d87a5c2ef
Arturo <olmos.arthur@gmail.com>
2018-12-22 18:25:38 +0100

    no message

--- lastpotatodev/settings.py
+++ lastpotatodev/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_databas_url
+import dj_database_url
 
-db_from_env= dj_databas_url.config()
+db_from_env= dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
