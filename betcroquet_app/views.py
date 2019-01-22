4e1ca05a9bef80c6999f290ec571dd725881b9b2
Arturo <olmos.arthur@gmail.com>
2019-01-22 19:24:50 +0100

    actualizado pantalla de inicio y añadido enlaces a login y registro

--- betcroquet_app/views.py
+++ betcroquet_app/views.py
@@ -6,6 +6,14 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+from user_mgmt_app.models import  UserProfileInfo,FriendRelationship
+from user_mgmt_app.forms import  FriendForm
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        user_data =  UserProfileInfo.objects.filter(user=request.user).values_list('profile_pic',flat=True)
+        pic= user_data[0]
+        request.session['profile_pic'] = pic
+
     return render(request,'betcroquet_app/index.html')
